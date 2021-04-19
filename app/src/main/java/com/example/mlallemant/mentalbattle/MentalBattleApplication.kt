4d1cca304bd0b310516b1e08e48c44c3c6adbc7e
zakaria riahi idrissi <zakaria.riahiidrissi.partner@decathlon.com>
2021-04-19 15:53:13 +0200

    Adding remote config wip

---
+++ app/src/main/java/com/example/mlallemant/mentalbattle/MentalBattleApplication.kt
@@ -0,0 +1,43 @@
+package com.example.mlallemant.mentalbattle
+
+import android.app.Application
+import android.util.Log
+import com.google.firebase.FirebaseApp
+import com.google.firebase.remoteconfig.FirebaseRemoteConfig
+import com.google.firebase.remoteconfig.FirebaseRemoteConfigSettings
+
+class MentalBattleApplication: Application() {
+
+    override fun onCreate() {
+        super.onCreate()
+        initFirebaseRemoteConfig()
+    }
+
+    private fun initFirebaseRemoteConfig() {
+        FirebaseApp.initializeApp(this)
+        FirebaseRemoteConfig.getInstance().apply {
+            //set this during development
+            val configSettings = FirebaseRemoteConfigSettings.Builder()
+                .setMinimumFetchIntervalInSeconds(0)
+                .build()
+            setConfigSettingsAsync(configSettings)
+            //set this during development
+            setDefaultsAsync(R.xml.remote_config_defaults)
+            fetchAndActivate().addOnCompleteListener { task ->
+                val updated = task.result
+                if (task.isSuccessful) {
+                    val updated = task.result
+                    Log.d("TAG", "Config params updated: $updated")
+                } else {
+                    Log.d("TAG", "Config params updated: $updated")
+                }
+            }
+        }
+    }
+
+    companion object {
+        const val REMOTE_CONFIG_TEST_VALUE = "test_value"
+        const val REMOTE_CONFIG_SIGNIN_WITH_GOOGLE = "signin_with_google"
+        const val REMOTE_CONFIG_SIGNIN_WITH_FACEBOOK = "signin_with_facebook"
+    }
+}
--- app/src/main/java/com/example/mlallemant/mentalbattle/ui/SplashActivity.kt
+++ app/src/main/java/com/example/mlallemant/mentalbattle/ui/SplashActivity.kt
@@ -2,9 +2,14 @@ package com.example.mlallemant.mentalbattle.ui
 
 import android.content.Intent
 import android.os.Bundle
+import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
+import com.example.mlallemant.mentalbattle.MentalBattleApplication.Companion.REMOTE_CONFIG_SIGNIN_WITH_FACEBOOK
+import com.example.mlallemant.mentalbattle.MentalBattleApplication.Companion.REMOTE_CONFIG_SIGNIN_WITH_GOOGLE
+import com.example.mlallemant.mentalbattle.MentalBattleApplication.Companion.REMOTE_CONFIG_TEST_VALUE
 import com.example.mlallemant.mentalbattle.databinding.SplashActivityBinding
 import com.example.mlallemant.mentalbattle.ui.login.LoginActivity
+import com.google.firebase.remoteconfig.FirebaseRemoteConfig
 import io.reactivex.Observable
 import io.reactivex.android.schedulers.AndroidSchedulers
 import io.reactivex.disposables.CompositeDisposable
@@ -22,6 +27,8 @@ class SplashActivity : AppCompatActivity() {
         _binding = SplashActivityBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        checkRemoteConfig()
+
         compositeDisposable.add(Observable.timer(SPLASH_TIME_OUT, TimeUnit.MILLISECONDS)
                 .subscribeOn(Schedulers.io())
                 .observeOn(AndroidSchedulers.mainThread())
@@ -36,6 +43,19 @@ class SplashActivity : AppCompatActivity() {
         super.onDestroy()
         _binding = null
         compositeDisposable.dispose()
+    }
+
+    private fun checkRemoteConfig() {
+        val remoteConfig = FirebaseRemoteConfig.getInstance()
+
+        val testValue = remoteConfig.getString(REMOTE_CONFIG_TEST_VALUE)
+        val signinWithGoogle = remoteConfig.getBoolean(REMOTE_CONFIG_SIGNIN_WITH_GOOGLE)
+        val signinWithFacebook = remoteConfig.getBoolean(REMOTE_CONFIG_SIGNIN_WITH_FACEBOOK)
+
+        Log.i(javaClass.simpleName, REMOTE_CONFIG_TEST_VALUE + testValue)
+        Log.i(javaClass.simpleName, REMOTE_CONFIG_SIGNIN_WITH_GOOGLE + signinWithGoogle)
+        Log.i(javaClass.simpleName, REMOTE_CONFIG_SIGNIN_WITH_FACEBOOK + signinWithFacebook)
+
     }
 
     companion object {
